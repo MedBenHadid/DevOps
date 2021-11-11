76155c232c4537cdf35efd85cdf63096af8fbbab
MedBenHadid <benhdid21@gmail.com>
2021-11-12 00:13:28 +0100

    Pre pull

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -68,14 +68,11 @@ pipeline {
             deleteDir()
         }
         failure {
-            mail to: 'chihab.hajji@esprit.tn',
+            mail to: 'mohamed.benhadid@esprit.tn',
                     subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
                     body: "Something is wrong with ${env.BUILD_URL}'s test"
-            discordSend description: "${IMAGE}:${VERSION} Pipeline Build", footer: "Build successful", link: env.BUILD_URL, result: currentBuild.currentResult, title: JOB_NAME, webhookURL: "https://discord.com/api/webhooks/908327603428028447/WqNlqvRQhP2caIzVFKOoItlXZa7yJIXiQUVjrKIfGRfhU_W184n_zfm2uZGQZOeE1Oba"
 
         }
-        success{
-            discordSend description: "${IMAGE}:${VERSION} Pipeline Build", footer: "Build successful", link: env.BUILD_URL, result: currentBuild.currentResult, title: JOB_NAME, webhookURL: "https://discord.com/api/webhooks/908327603428028447/WqNlqvRQhP2caIzVFKOoItlXZa7yJIXiQUVjrKIfGRfhU_W184n_zfm2uZGQZOeE1Oba"
-        }
+
     }
 }
