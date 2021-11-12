092800637f4aa2a0f03badf801e0b461b1063420
MedBenHadid <benhdid21@gmail.com>
2021-11-12 17:07:09 +0100

    pom change version

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -30,6 +30,7 @@ pipeline {
                 withMaven{
                     sh "mvn -B org.jacoco:jacoco-maven-plugin:prepare-agent-integration failsafe:integration-test failsafe:verify -DskipTests"
                 }
+//                step([$class: 'JUnitResultArchiver', testResults: '**/target/failsafe-reports/TEST-*.xml'])
             }
         }
         stage('Build'){
